c551b6116f234fd2e8e341353c56eb94b033a481
Vladislav Zavjalov <slazav@altlinux.org>
2015-12-14 20:33:12 +0000

    update polar_dealii/polar.cc

--- polar_dealii/polar.cc
+++ polar_dealii/polar.cc
@@ -29,28 +29,13 @@
 
 #include <deal.II/lac/eigen.h>
 
-
+#include <deal.II/lac/arpack_solver.h>
 
 #include <fstream>
 #include <sstream>
 #include <iostream>
 
 using namespace dealii;
-
-/* my boundary function: N*pi between vortices, 0 on other sides */
-class BoundaryValues : public Function<2>{
-    double R; // radius between vortices
-    double N; // vortex winding
-
-  public:
-    BoundaryValues(const double R_, const double N_=0.5) :
-      Function<2>(), R(R_), N(N_){}
-
-    double value(const Point<2> &p, const unsigned int ) const{
-      return p(1)<R && p(0)<R ? N*M_PI:0.0; }
-};
-
-
 
 template <int dim>
 class Polar {
@@ -82,14 +67,14 @@ class Polar {
 
 template <int dim>
 Polar<dim>::Polar() : dof_handler(triangulation), fe(2) {
-  R=3;  Lx=5.0;  Ly=5.0;
+  R=14;  Lx=20.0;  Ly=20.0;
 }
 
 template <int dim> Polar<dim>::~Polar() { dof_handler.clear(); }
 
 template <int dim>
 void Polar<dim>::run(){
-  for (unsigned int i=0; i<10; ++i){
+  for (unsigned int i=0; i<15; ++i){
     int stage = i<8? 1:2;
     if (i==0) stage=0;
     run_step(stage, i);
@@ -105,20 +90,12 @@ void Polar<dim>::run_step(const int stage, const int step){
   if (stage==0){ // first step
     GridGenerator::hyper_rectangle(triangulation,
       Point<2>(0,0), Point<2>(Lx,Ly));
-    triangulation.refine_global(2);
+    triangulation.refine_global(4);
 
-    // set cell indicators X=0
-    typename Triangulation<dim>::cell_iterator
-      cell = triangulation.begin(),
-      endc = triangulation.end();
-    for (; cell!=endc; ++cell){
-      for (unsigned int i=0; i<GeometryInfo<dim>::faces_per_cell; i++){
-        if (std::fabs(cell->face(i)->center()(0)) < 1e-12)
-          cell->face(i)->set_boundary_id(1);
-      }
-    }
-
+    // enumerate degrees of freedom
     dof_handler.distribute_dofs(fe);
+
+    // init the texture vector
     texture.reinit(dof_handler.n_dofs());
   }
 
@@ -135,7 +112,7 @@ void Polar<dim>::run_step(const int stage, const int step){
 
     // mark cells for refinement(30% worse to refine, 3%best to coarse)
     GridRefinement::refine_and_coarsen_fixed_number(triangulation,
-                                    estimated_error_per_cell, 0.3, 0.03);
+                                    estimated_error_per_cell, 0.1, 0.5	);
     // mark additional cells to be refined
     triangulation.prepare_coarsening_and_refinement();
 
@@ -146,6 +123,7 @@ void Polar<dim>::run_step(const int stage, const int step){
     // do actual rifinement
     triangulation.execute_coarsening_and_refinement();
 
+    // enumerate degrees of freedom
     dof_handler.distribute_dofs(fe);
 
     // do transfer of the solution to the new mesh
@@ -154,7 +132,27 @@ void Polar<dim>::run_step(const int stage, const int step){
     texture = tmp;
   }
 
-  if (stage==3)  wave.reinit(dof_handler.n_dofs());
+  // reset boundary IDs
+  if (stage==0 || stage==1) {
+    // set cell indicators X=0
+    typename Triangulation<dim>::cell_iterator
+      cell = triangulation.begin(),
+      endc = triangulation.end();
+    for (; cell!=endc; ++cell){
+      for (unsigned int i=0; i<GeometryInfo<dim>::faces_per_cell; i++){
+        // x=0 ID=1
+        if (std::fabs(cell->face(i)->center()(0)) < 1e-12)
+          cell->face(i)->set_boundary_id(1);
+        else if (std::fabs(cell->face(i)->center()(0)) < R &&
+                 std::fabs(cell->face(i)->center()(1)) < 1e-12)
+          cell->face(i)->set_boundary_id(2);
+      }
+    }
+  }
+
+
+  // init the wave vector at all stages
+  wave.reinit(dof_handler.n_dofs());
 
   // set constraints
   if (stage==0 || stage==1){
@@ -162,7 +160,10 @@ void Polar<dim>::run_step(const int stage, const int step){
     DoFTools::make_hanging_node_constraints(dof_handler,
                                              constraints);
     VectorTools::interpolate_boundary_values(dof_handler, 0,
-                                              BoundaryValues(R),
+                                              ZeroFunction<dim>(),
+                                              constraints);
+    VectorTools::interpolate_boundary_values(dof_handler, 2,
+                                              ConstantFunction<dim>(0.5*M_PI),
                                               constraints);
     constraints.close();
   }
@@ -173,6 +174,12 @@ void Polar<dim>::run_step(const int stage, const int step){
     DoFTools::make_hanging_node_constraints(dof_handler,
                                              constraints);
     VectorTools::interpolate_boundary_values(dof_handler, 0,
+                                              ZeroFunction<dim>(),
+                                              constraints);
+    VectorTools::interpolate_boundary_values(dof_handler, 1,
+                                              ZeroFunction<dim>(),
+                                              constraints);
+    VectorTools::interpolate_boundary_values(dof_handler, 2,
                                               ZeroFunction<dim>(),
                                               constraints);
     constraints.close();
@@ -186,8 +193,8 @@ void Polar<dim>::run_step(const int stage, const int step){
 
   B.reinit(dof_handler.n_dofs());
   DynamicSparsityPattern dsp(dof_handler.n_dofs());
-  DoFTools::make_sparsity_pattern(dof_handler, dsp);
-  constraints.condense(dsp);
+  DoFTools::make_sparsity_pattern(dof_handler, dsp, constraints, false);
+  //constraints.condense(dsp);
   sparsity_pattern.copy_from(dsp);
   A.reinit(sparsity_pattern);
 
@@ -206,8 +213,6 @@ void Polar<dim>::run_step(const int stage, const int step){
   // texture calculation
   if (stage==0 || stage==1 || stage==2){
 
-    double v = step<6? step/6.0:1.0;
-
     std::vector<double> texture_cell(nq);
 
     typename DoFHandler<dim>::active_cell_iterator
@@ -220,16 +225,23 @@ void Polar<dim>::run_step(const int stage, const int step){
       fe_values.reinit(cell);
       fe_values.get_function_values(texture, texture_cell);
 
+      // here I solve: nabla^2 Un+1 - Un+1 = sin(2Un)/2 - Un
       for (unsigned int q=0; q<nq; ++q){
         for (unsigned int i=0; i<nd; ++i){
           for (unsigned int j=0; j<nd; ++j){
-            Acell(i,j) +=(fe_values.shape_grad(i,q) *
+            Acell(i,j) -=(fe_values.shape_grad(i,q) *
                           fe_values.shape_grad(j,q) *
                           fe_values.JxW(q));
+            Acell(i,j) -=(fe_values.shape_value(i,q) *
+                          fe_values.shape_value(j,q) *
+                          fe_values.JxW(q));
           }
-          Bcell(i) += -(fe_values.shape_value(i,q) *
-                        0.5*v*sin(2.0*texture_cell[q]) *
-                        fe_values.JxW(q));
+          Bcell(i) += (fe_values.shape_value(i,q) *
+                       0.5*sin(2.0*texture_cell[q]) *
+                       fe_values.JxW(q));
+          Bcell(i) -= (fe_values.shape_value(i,q) *
+                       texture_cell[q] *
+                       fe_values.JxW(q));
         }
       }
       // Finally, transfer the contributions from Acell and
@@ -240,10 +252,11 @@ void Polar<dim>::run_step(const int stage, const int step){
     }
   }
 
-  // texture calculation
+  // wave calculation
   if (stage==3){
 
     std::vector<double> texture_cell(nq);
+    std::vector<Tensor<1,dim> > tgrad_cell(nq);
 
     typename DoFHandler<dim>::active_cell_iterator
     cell = dof_handler.begin_active(),
@@ -254,19 +267,29 @@ void Polar<dim>::run_step(const int stage, const int step){
 
       fe_values.reinit(cell);
       fe_values.get_function_values(texture, texture_cell);
+      fe_values.get_function_gradients(texture, tgrad_cell);
 
       for (unsigned int q=0; q<nq; ++q){
         for (unsigned int i=0; i<nd; ++i){
           for (unsigned int j=0; j<nd; ++j){
-            Acell(i,j) -=(fe_values.shape_grad(i,q) *
+            Acell(i,j) +=(fe_values.shape_grad(i,q) *
                           fe_values.shape_grad(j,q) *
                           fe_values.JxW(q));
 
-            Acell(i,j) +=(fe_values.shape_value(i,q) *
-                          fe_values.shape_value(j,q) *
-                          pow(sin(texture_cell[q]),2) *
-                          fe_values.JxW(q));
+//            Acell(i,j) -=(fe_values.shape_value(i,q) *
+//                          fe_values.shape_value(j,q) *
+//                          pow(sin(texture_cell[q]),2)*
+//                          fe_values.JxW(q));
+//
+//            Acell(i,j) +=(fe_values.shape_value(i,q) *
+//                	  fe_values.shape_value(j,q) *
+//                          tgrad_cell[q].norm_square() *
+//                          fe_values.JxW(q));
+
           }
+          Bcell(i) += (fe_values.shape_value(i,q) *
+                       0.1 *
+                       fe_values.JxW(q));
         }
       }
       // Finally, transfer the contributions from Acell and
@@ -288,22 +311,28 @@ void Polar<dim>::run_step(const int stage, const int step){
     preconditioner.initialize(A, 1.2);
     solver.solve(A, texture, B, preconditioner);
     constraints.distribute(texture);
-    // TODO: estimate error
   }
 
   if (stage==3){
-    for (unsigned int i=0; i<texture.size(); i++){
-      wave[i]=pow(sin(texture[i]),2);
-    }
+    SolverControl      solver_control(10000, 1e-6);
+//    SolverCG<>         solver1(solver_control);
 
-//    SolverControl      solver_control(1000, 1e-12);
-//    PrimitiveVectorMemory<> mem;
-//    EigenPower<>::AdditionalData data;
-//    data.shift=1000;
-//    EigenPower<>       solver(solver_control,mem, data);
-//    solver.solve(en, A, wave);
+//    PreconditionSSOR<> preconditioner;
+//    preconditioner.initialize(A, 1.2);
+//    solver1.solve(A, wave, B, preconditioner);
 //    constraints.distribute(wave);
-//    std::cout << "En: " << en << "\n";
+
+    for (unsigned int i = 0; i<wave.size(); i++) wave[i]=1;
+
+    en=-10.01;
+    GrowingVectorMemory<> mem;
+    EigenInverse<>::AdditionalData data;
+    data.relaxation = 0.01;
+    data.use_residual = false;
+    EigenInverse<> solver2(solver_control,mem, data);
+    solver2.solve(en, A, wave);
+    constraints.distribute(wave);
+    std::cout << "En: " << en << "\n";
   }
 
   // write grid
@@ -336,7 +365,7 @@ void Polar<dim>::run_step(const int stage, const int step){
     data_out.write_eps(out2);
   }
 
-  // write texture
+  // write wave
   if (stage==3){
     DataOut<dim> data_out;
     data_out.set_flags(eps_flags);
@@ -351,10 +380,35 @@ void Polar<dim>::run_step(const int stage, const int step){
     data_out.write_eps(out2);
   }
 
+
+  double It=0, Iw=0;
+  if (stage==0 || stage==1 || stage==2){
+    // estimate error (wave is used for the difference)
+    Vector<float> difference_per_cell(triangulation.n_active_cells());
+    VectorTools::integrate_difference (dof_handler,
+      texture, ZeroFunction<dim>(), difference_per_cell,
+      QGauss<dim>(3),VectorTools::L2_norm);
+    It = difference_per_cell.l2_norm();
+  }
+
+  if (stage==3){
+    // estimate error (wave is used for the difference)
+    Vector<float> difference_per_cell(triangulation.n_active_cells());
+    VectorTools::integrate_difference (dof_handler,
+      wave, ZeroFunction<dim>(), difference_per_cell,
+      QGauss<dim>(3),VectorTools::L2_norm);
+    Iw = difference_per_cell.l2_norm();
+  }
+
+
+
+
   // print some information about the cell and dof
   std::cout << "  step: " << std::setw(3) << step
             << "  act.cells: " << std::setw(4) << triangulation.n_active_cells()
             << "  DOF: " << std::setw(4) << dof_handler.n_dofs()
+            << "  T " << It
+            << "  W " << Iw
             << std::endl;
 
 
